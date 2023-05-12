cc3efca76e8ae789d5d70a87a7bc54175d31ac4e
jerodev <jeroen@deviaene.eu>
2023-05-12 11:50:49 +0200

    Test class aliases

---
+++ tests/_Mocks/Aliases.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jerodev\DataMapper\Tests\_Mocks;
+
+use Jerodev\DataMapper\Tests\_Mocks\UserDto as GebruikerObject;
+
+class Aliases
+{
+    /** @var array<string, GebruikerObject> */
+    public array $userAliases;
+}
